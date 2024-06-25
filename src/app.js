34e9c49524200f07108eaae2068ece4c14195863
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-06-25 16:23:41 +0545

    hello

--- src/app.js
+++ src/app.js
@@ -1,14 +1,13 @@
 import express from "express";
 import cors from "cors";
 
-
 //import routes
 import studentRouter from "./routes/student.routes.js";
 import QueryRouter from "./routes/query.routes.js";
 const app = express();
 app.use(
   cors({
-    origin: [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN1, process.env.CORS_ORIGIN2],
+    origin: "https://learn-with-biraj-clientv1.vercel.app/",
   })
 );
 app.use(express.json());
