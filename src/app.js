ec9bb8945b4342a00bf249b9f379b9a7e3a425ba
Biraj Baral <117191235+pointbreak6969@users.noreply.github.com>
2024-07-07 07:16:54 +0545

    style

--- src/app.js
+++ src/app.js
@@ -6,7 +6,9 @@ import studentRouter from "./routes/student.routes.js";
 import QueryRouter from "./routes/query.routes.js";
 import adminRouter from "./routes/admin.routes.js";
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin : [process.env.CORS_ORIGIN, process.env.CORS_ORIGIN1]
+}));
 app.use(express.json());
 app.use("/api/v1/students", studentRouter);
 app.use("/api/v1/query", QueryRouter);
